2cc7ea28a3dd86d0db07a1605d9ed4af9ba80bdf
jongmin oh <113002267+sejongmin@users.noreply.github.com>
2024-01-31 15:19:37 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.06 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.py
+++ 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.py
@@ -1,11 +1,7 @@
 def solution(n):
     answer = 0
-    i = 0
-    while True:
-        if i == n:
-            break
-            
-        i += 1
+    
+    for i in range(n):
         answer += 1
         while answer % 3 == 0 or 3 in list(map(int, str(answer))):
             answer += 1
