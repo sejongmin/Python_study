1cbd1551cee57726e7da765984707248385d296f
jongmin oh <113002267+sejongmin@users.noreply.github.com>
2024-02-02 13:51:27 +0900

    [level 2] Title: 하노이의 탑, Time: 15.97 ms, Memory: 18.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.py
@@ -0,0 +1,12 @@
+def hanoi(n, a, b, c, answer):
+    if n == 1:
+        answer.append([a, c])
+        return
+    hanoi(n - 1, a, c, b, answer)
+    answer.append([a, c])
+    hanoi(n - 1, b, a, c, answer)
+
+def solution(n):
+    answer = []
+    hanoi(n, 1, 2, 3, answer)
+    return answer
