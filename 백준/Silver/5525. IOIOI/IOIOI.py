ce3fe7f97985e618cbc5608dc12de84f3aeadfce
sejongmin <113002267+sejongmin@users.noreply.github.com>
2024-08-22 15:29:57 +0900

    [Silver I] Title: IOIOI, Time: 208 ms, Memory: 33076 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -3,21 +3,21 @@ input = sys.stdin.readline
 
 def solution(N, L, S):
     answer = 0
-    arr = list(S)
-    label = ['I', 'O']
-    for i in range(L - 2 * N):
-        o = False
-        if arr[i] == arr[i + 2 * N] == label[o]:
-            flag = True
-            for j in range(1, N + 1):
-                o = not o
-                if arr[i + j] == arr[i + 2 * N - j] == label[o]:
-                    continue
-                else:
-                    flag=False
-                    break
-            if flag:
+    ioi = "IOI"
+
+    i = 0
+    count = 0
+    while i < L - 2:
+        if S[i:i+3] == ioi:
+            count += 1
+            i += 2
+            if count == N:
                 answer += 1
+                count -= 1
+        else:
+            count = 0
+            i += 1
+    
     return answer
 
 N = int(input())
