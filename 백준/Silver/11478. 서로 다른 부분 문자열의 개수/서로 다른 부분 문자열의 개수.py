2936a2fdb42ba946dc5437121376072d4f95f1bc
sejongmin <113002267+sejongmin@users.noreply.github.com>
2024-09-16 22:16:56 +0900

    서로 다른 부분 문자열의 개수.py 업데이트

--- 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.py
+++ 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.py
@@ -3,8 +3,9 @@ input = sys.stdin.readline
 
 def solution(S):
     sub_str = set()
-    for i in range(1, len(S) + 1):
-        for j in range(len(S) - i + 1):
+    len_str = len(S)
+    for i in range(1, len_str + 1):
+        for j in range(len_str - i + 1):
             sub_str.add(S[j:j+i])
     return len(sub_str)
 
