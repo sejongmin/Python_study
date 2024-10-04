ac539b767301b9acf13ff1634f1f85004320ec09
sejongmin <113002267+sejongmin@users.noreply.github.com>
2024-10-04 23:41:09 +0900

    $A ＋ B － C$.py 업데이트

--- 백준/Bronze/31403. $A ＋ B － C$/$A ＋ B － C$.py
+++ 백준/Bronze/31403. $A ＋ B － C$/$A ＋ B － C$.py
@@ -2,10 +2,10 @@ import sys
 input = sys.stdin.readline
 
 def solution(A, B, C):
-    print(int(A) + int(B) - int(C))
-    print(int(A + B) - int(C))
+    print(int(A) + int(B) - C)
+    print(int(A + B) - C)
 
 A = input().strip()
 B = input().strip()
-C = input().strip()
+C = int(input())
 solution(A, B, C)
