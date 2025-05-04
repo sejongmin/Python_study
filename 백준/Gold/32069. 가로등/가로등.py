07dea10295f1edd25feabbab5eb93b5c4999a415
sejongmin <113002267+sejongmin@users.noreply.github.com>
2025-05-05 00:05:19 +0900

    [Gold V] Title: 가로등, Time: 944 ms, Memory: 193616 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/32069. 가로등/가로등.py
+++ 백준/Gold/32069. 가로등/가로등.py
@@ -1,15 +1,27 @@
 import sys
+from collections import deque
 input = sys.stdin.readline
 
 def solution(L, N, K, arr):
     answer = []
-    for i in range(L + 1):
-        now = L + 1
-        for j in range(N):
-            if now > abs(i - arr[j]):
-                now = abs(i - arr[j])
-        answer.append(now)
-    print(*sorted(answer)[:K], sep='\n')
+    q = deque()
+    visited = set()
+    for i in range(N):
+        visited.add(arr[i])
+        q.append((arr[i], 0))
+
+    cnt = 0
+    while cnt < K:
+        cnt += 1
+        now, dark = q.popleft()
+        if now - 1 not in visited and now > 0:
+            visited.add(now - 1)
+            q.append((now - 1, dark + 1))
+        if now + 1 not in visited and now < L:
+            visited.add(now + 1)
+            q.append((now + 1, dark + 1))
+        answer.append(dark)
+    print(*answer, sep="\n")
 
 L, N, K = map(int, input().split())
 arr = list(map(int, input().split()))
