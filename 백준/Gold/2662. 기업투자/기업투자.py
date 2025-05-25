bc0fdc75ccbe0bba8a369f1a840354c4316a980a
sejongmin <113002267+sejongmin@users.noreply.github.com>
2025-05-25 11:23:45 +0900

    [Gold II] Title: 기업투자, Time: 100 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/2662. 기업투자/기업투자.py
+++ 백준/Gold/2662. 기업투자/기업투자.py
@@ -5,7 +5,6 @@ def solution(N, M, companys):
     dp = [0 for _ in range(N + 1)]
     invest = [[0 for _ in range(M)] for i in range(N + 1)] # 총투자액 -> 기업
 
-    now = 0
     for i in range(N):
         dp[i + 1] = companys[i][1]
         invest[i + 1][0] = i + 1
@@ -20,7 +19,6 @@ def solution(N, M, companys):
 
     print(dp[-1])
     print(*invest[-1])
-        
 
 N, M = map(int, input().split())
 companys = [list(map(int, input().split())) for _ in range(N)]
