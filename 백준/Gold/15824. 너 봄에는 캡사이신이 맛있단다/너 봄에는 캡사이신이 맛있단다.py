fcb53c28bca8d305c905952960f61af385a5b83c
sejongmin <113002267+sejongmin@users.noreply.github.com>
2025-07-02 01:22:21 +0900

    [Gold II] Title: 너 봄에는 캡사이신이 맛있단다, Time: 412 ms, Memory: 159492 KB, Score: 250 point -BaekjoonHub

--- 백준/Gold/15824. 너 봄에는 캡사이신이 맛있단다/너 봄에는 캡사이신이 맛있단다.py
+++ 백준/Gold/15824. 너 봄에는 캡사이신이 맛있단다/너 봄에는 캡사이신이 맛있단다.py
@@ -5,12 +5,10 @@ MOD = int(1e9) + 7
 def solution(N, menu):
     answer = 0
     menu.sort()
-    pow_cache = [1] * N
-    for i in range(1, N):
-        pow_cache[i] = pow_cache[i - 1] * 2
 
     for i in range(N):
-        answer += (menu[i] * (pow_cache[i] - pow_cache[N - 1 - i])) % MOD
+        answer += (menu[i] * (pow(2, i, MOD) - pow(2, N - 1 - i, MOD)))
+        answer %= MOD
     
     return answer % MOD
 
